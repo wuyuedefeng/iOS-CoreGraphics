82a11c646bd171dfa2c080acc4b35618b9e7c32b
wangsen <wangs@tongxinyiliao.com>
2015-06-01 21:47:07 +0800

    Initial Commit

---
+++ CGGraphicsLearn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CGGraphicsLearn
+//
+//  Created by senwang on 15/6/1.
+//  Copyright (c) 2015年 senwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CGGraphicsLearn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CGGraphicsLearn
+//
+//  Created by senwang on 15/6/1.
+//  Copyright (c) 2015年 senwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
